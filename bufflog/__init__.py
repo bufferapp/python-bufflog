3a1cd446d2013e1c477c817088f49ccb5c9adae5
David Gasquez <davidgasquez@gmail.com>
2020-10-21 13:15:54 +0200

    Disable ddtrace

--- bufflog/__init__.py
+++ bufflog/__init__.py
@@ -3,13 +3,13 @@ import logging
 import os
 import sys
 
-try:
-    import ddtrace
-    from ddtrace.helpers import get_correlation_ids
+# try:
+#     import ddtrace
+#     from ddtrace.helpers import get_correlation_ids
 
-    ddtrace_available = True
-except ImportError:
-    ddtrace_available = False
+#     ddtrace_available = True
+# except ImportError:
+#     ddtrace_available = False
 
 from structlog.processors import JSONRenderer
 from structlog.stdlib import filter_by_level
@@ -18,20 +18,20 @@ from structlog.stdlib import add_log_level_number
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
 
 
-def tracer_injection(logger, log_method, event_dict):
-    # get correlation ids from current tracer context
-    trace_id, span_id = get_correlation_ids()
+# def tracer_injection(logger, log_method, event_dict):
+#     # get correlation ids from current tracer context
+#     trace_id, span_id = get_correlation_ids()
 
-    # add ids to structlog event dictionary
-    event_dict["dd.trace_id"] = trace_id or 0
-    event_dict["dd.span_id"] = span_id or 0
+#     # add ids to structlog event dictionary
+#     event_dict["dd.trace_id"] = trace_id or 0
+#     event_dict["dd.span_id"] = span_id or 0
 
-    # add the env, service, and version configured for the tracer
-    event_dict["dd.env"] = ddtrace.config.env or ""
-    event_dict["dd.service"] = ddtrace.config.service or ""
-    event_dict["dd.version"] = ddtrace.config.version or ""
+#     # add the env, service, and version configured for the tracer
+#     event_dict["dd.env"] = ddtrace.config.env or ""
+#     event_dict["dd.service"] = ddtrace.config.service or ""
+#     event_dict["dd.version"] = ddtrace.config.version or ""
 
-    return event_dict
+#     return event_dict
 
 
 def rename_message_key(_, __, event_dict):
@@ -58,8 +58,8 @@ processors = [
     JSONRenderer(),
 ]
 
-if ddtrace_available:
-    processors.insert(0, tracer_injection)
+# if ddtrace_available:
+#     processors.insert(0, tracer_injection)
 
 structlog.configure(
     context_class=dict,
@@ -69,5 +69,5 @@ structlog.configure(
     processors=processors,
 )
 
-bufflog = structlog.get_logger()
-bufflog._logger.setLevel(LOG_LEVEL)
+# bufflog = structlog.get_logger()
+# bufflog._logger.setLevel(LOG_LEVEL)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python-bufflog",
-    version="0.0.4",
+    version="0.0.5",
     author="David Gasquez",
     author_email="davidgasquez@buffer.com",
     description="Python logger for Buffer services",
